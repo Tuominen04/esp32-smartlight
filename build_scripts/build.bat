d77fac3c165b9c39c75fd6ef3f817fde9e6a18bb
Tuominen <81805500+Tuominen04@users.noreply.github.com>
2026-04-03 22:50:41 +0300

    remove build scritps dir

--- build_scripts/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting up ESP-IDF environment...
-
-call %IDF_PATH%\export.bat
-
-echo Cleaning and building the project...
-idf.py clean build
--- build_scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Setting up ESP-IDF environment..."
-
-# Use the IDF_PATH environment variable to source ESP-IDF
-source "$IDF_PATH/export.sh"
-
-echo "Clean and building the project..."
-idf.py clean build
